44253153faaaf7453888a2adae0badefea03e800
MarlomX <117599951+MarlomX@users.noreply.github.com>
2023-11-08 17:53:24 -0300

    BancoDeDados

--- src/conexaoBD.php
+++ src/conexaoBD.php
@@ -1,3 +1,4 @@
 <?php
-    $pdo = new PDO('mysql:host=localhost;dbname=purple_filmes', 'root', 'MarlomRoot1987');
+    //Mudar Usuario e Senha para o local respectivo onde estiver o Banco de dados
+    $pdo = new PDO('mysql:host=localhost;dbname=purple_filmes', 'Usuario', 'Senha');
 ?>
